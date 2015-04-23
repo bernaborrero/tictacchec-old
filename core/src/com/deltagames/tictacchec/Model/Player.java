331cfbb52a72b8906f47df6fd48f7169de9a22fc
maxi <mvillanu@infobosccoma.net>
2015-04-23 18:45:08 +0200

    Class Coordinates added

--- core/src/com/deltagames/tictacchec/Model/Player.java
+++ core/src/com/deltagames/tictacchec/Model/Player.java
@@ -10,15 +10,23 @@ package com.deltagames.tictacchec.Model;
 
     }
 
-    public boolean hasWon(Cheap[][] board, Coordenates cheapMoved){
+    public boolean hasWon(Cheap[][] board, Coordinates cheapMoved){
         if(cheapMoved.getX()==0){
-            int i=0;
-            boolean valid=true;
-            while(i<4&&valid){
 
-            }
         }
 
         return false;
+    }
+
+
+    private boolean checkVertically(Cheap[][] board, Coordinates cheapMoved){
+        int i=0;
+        boolean valid=true;
+        while(i<4&&valid){
+            if(board[cheapMoved.getY()][i].get)
+                if(valid){
+                    i++;
+                }
+        }
     }
 }
