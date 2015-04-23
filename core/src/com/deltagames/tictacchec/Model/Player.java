88fa4eaa084c963fc750940db5d9ccf445357b09
maxi <mvillanu@infobosccoma.net>
2015-04-23 18:22:02 +0200

    Model Package created

---
+++ core/src/com/deltagames/tictacchec/Model/Player.java
@@ -0,0 +1,15 @@
+package com.deltagames.tictacchec.Model;
+
+/**
+ * Created by Maxi on 23/04/2015.
+ */
+    public class Player {
+
+    public Player(){
+
+    }
+
+    public boolean hasWon(){
+        return false;
+    }
+}
