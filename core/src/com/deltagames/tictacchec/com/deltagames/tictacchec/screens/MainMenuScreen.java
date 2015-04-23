69bac9b1d909f904de15ead2bdaa5858c25422d9
Bernabé Borrero <bernabeborrero@gmail.com>
2015-04-23 17:12:44 +0200

    Created basic main menu screen

--- core/src/com/deltagames/tictacchec/com/deltagames/tictacchec/screens/MainMenuScreen.java
+++ core/src/com/deltagames/tictacchec/com/deltagames/tictacchec/screens/MainMenuScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.deltagames.tictacchec.TicTacChec;
@@ -16,6 +17,7 @@ import com.deltagames.tictacchec.TicTacChec;
 public class MainMenuScreen extends BaseScreen {
 
     private Stage stage;
+    private Table table;
 
     private Skin skin;
     private Label titleLabel;
@@ -24,8 +26,9 @@ public class MainMenuScreen extends BaseScreen {
     public MainMenuScreen(TicTacChec game) {
         super(game);
         stage = new Stage();
+        table = new Table();
 
-        skin = new Skin(Gdx.files.internal(""));
+        skin = new Skin(Gdx.files.internal("skins/default.json"));
 
         titleLabel = new Label("Tic Tac Chec", skin);
         playWithComputerButton = new TextButton("Play With Computer", skin);
@@ -66,7 +69,21 @@ public class MainMenuScreen extends BaseScreen {
     public void render(float delta) {
         super.render(delta);
 
+        stage.act();
+        stage.draw();
+    }
 
+    @Override
+    public void show() {
+        table.add(titleLabel).padBottom(40).row();
+        table.add(playWithComputerButton).size(150, 60).padBottom(20).row();
+        table.add(playWithPersonButton).size(150, 60).padBottom(20).row();
+        table.add(exitButton).size(150, 60).padBottom(20).row();
+
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
