c6e0462448709c19da5d52c0f49856069bbaa7de
Bernabé Borrero <bernabeborrero@gmail.com>
2015-04-24 19:49:07 +0200

    Moves class + fixes on Coordinates class

--- core/src/com/deltagames/tictacchec/Model/Coordinates.java
+++ core/src/com/deltagames/tictacchec/Model/Coordinates.java
@@ -1,9 +1,10 @@
 package com.deltagames.tictacchec.Model;
 
 /**
+ * Class to store a pair of coordinates
  * Created by Maxi on 23/04/2015.
  */
-public class Coordinates {
+public class Coordinates implements Comparable {
 
     private int x;
     private int y;
@@ -29,5 +30,27 @@ public class Coordinates {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Coordinates coordinates = (Coordinates) o;
+
+        if(coordinates.getY() < getY()) {
+            return 1;
+        }
+        else if(coordinates.getY() > getY()) {
+            return -1;
+        }
+        else {
+            if(coordinates.getX() < getX()) {
+                return 1;
+            }
+            else if(coordinates.getX() > getX()) {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 }
--- core/src/com/deltagames/tictacchec/Model/Moves.java
+++ core/src/com/deltagames/tictacchec/Model/Moves.java
@@ -11,13 +11,24 @@ public class Moves {
     private TreeMap<Coordinates, Boolean> moves;
 
     public Moves() {
-        moves = new TreeMap<>();
+        moves = new TreeMap<Coordinates, Boolean>();
     }
 
+    /**
+     * Add a new value to the collection
+     * @param coordinates the new coordinates to store
+     */
     public void add(Coordinates coordinates) {
         moves.put(coordinates, true);
     }
 
-
+    /**
+     * Checks if the collection contains a pair of coordinates
+     * @param coordinates the coordinates to check
+     * @return true if the collection contains the coordinates, false otherwise
+     */
+    public boolean hasMove(Coordinates coordinates) {
+        return moves.containsKey(coordinates);
+    }
 
 }
