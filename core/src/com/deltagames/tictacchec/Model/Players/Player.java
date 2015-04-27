411e3977a2946533c9d455bd5e1da1f7d3d7137f
Bernabé Borrero <bernabeborrero@gmail.com>
2015-04-27 16:27:16 +0200

    Make Player abstract

--- core/src/com/deltagames/tictacchec/Model/Players/Player.java
+++ core/src/com/deltagames/tictacchec/Model/Players/Player.java
@@ -5,25 +5,27 @@ import com.deltagames.tictacchec.Model.Board;
 import com.deltagames.tictacchec.Model.Color;
 import com.deltagames.tictacchec.Model.Coordinates;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * This class helps to handle a single player
  */
-    public class Player {
+public abstract class Player {
 
     private Color color;
     private int activeCheaps;
 
 
-    public Player(){
-        activeCheaps=0;
+    public Player() {
+        activeCheaps = 0;
     }
 
-    public void setActiveCheaps(int  activeCheaps){
-        this.activeCheaps=activeCheaps;
+    public void setActiveCheaps(int activeCheaps) {
+        this.activeCheaps = activeCheaps;
     }
 
-    public boolean hasWon(Board board, Coordinates cheapMoved){
-        if(checkVertically(board, cheapMoved)||checkHorizontal(board, cheapMoved)||checkDiagonal(board, cheapMoved)){
+    public boolean hasWon(Board board, Coordinates cheapMoved) {
+        if (checkVertically(board, cheapMoved) || checkHorizontal(board, cheapMoved) || checkDiagonal(board, cheapMoved)) {
             return true;
         }
 
@@ -31,15 +33,15 @@ import com.deltagames.tictacchec.Model.Coordinates;
     }
 
 
-    private boolean checkVertically(Board board, Coordinates cheapMoved){
-        int i=0;
-        boolean valid=true;
-        if(board!=null){
-            while(i<4&&valid){
-                if(board.get(new Coordinates(cheapMoved.getY(),i))==null || board.get(new Coordinates(cheapMoved.getY(),i)).getColor()!=this.getColor()){
-                    valid =false;
+    private boolean checkVertically(Board board, Coordinates cheapMoved) {
+        int i = 0;
+        boolean valid = true;
+        if (board != null) {
+            while (i < 4 && valid) {
+                if (board.get(new Coordinates(cheapMoved.getY(), i)) == null || board.get(new Coordinates(cheapMoved.getY(), i)).getColor() != this.getColor()) {
+                    valid = false;
                 }
-                if(valid){
+                if (valid) {
                     i++;
                 }
             }
@@ -47,15 +49,15 @@ import com.deltagames.tictacchec.Model.Coordinates;
         return valid;
     }
 
-    private boolean checkHorizontal(Board board, Coordinates cheapMoved){
-        int i=0;
-        boolean valid=true;
-        if(board!=null){
-            while(i<4&&valid){
-                if(board.get(new Coordinates(i,cheapMoved.getX()))==null || board.get(new Coordinates(i,cheapMoved.getX())).getColor()!=this.getColor()){
-                    valid =false;
+    private boolean checkHorizontal(Board board, Coordinates cheapMoved) {
+        int i = 0;
+        boolean valid = true;
+        if (board != null) {
+            while (i < 4 && valid) {
+                if (board.get(new Coordinates(i, cheapMoved.getX())) == null || board.get(new Coordinates(i, cheapMoved.getX())).getColor() != this.getColor()) {
+                    valid = false;
                 }
-                if(valid){
+                if (valid) {
                     i++;
                 }
             }
@@ -64,31 +66,41 @@ import com.deltagames.tictacchec.Model.Coordinates;
     }
 
 
-    private boolean checkDiagonal(Board board, Coordinates cheapMoved){
-        boolean valid=true;
-        int i=0;
+    private boolean checkDiagonal(Board board, Coordinates cheapMoved) {
+        boolean valid = true;
+        int i = 0;
 
-        while(i<4 && valid){
-            if(board.get(i,i)==null || board.get(i,i).getColor()!=this.getColor()){
-                valid=false;
+        while (i < 4 && valid) {
+            if (board.get(i, i) == null || board.get(i, i).getColor() != this.getColor()) {
+                valid = false;
             }
             i++;
         }
 
-        if(!valid){
-            valid=true;
-            i=3;
-            while(i>=0 && valid){
-                if(board.get(i,i)==null ||board.get(i,i).getColor()!=this.getColor()){
-                    valid=false;
+        if (!valid) {
+            valid = true;
+            i = 3;
+            while (i >= 0 && valid) {
+                if (board.get(i, i) == null || board.get(i, i).getColor() != this.getColor()) {
+                    valid = false;
                 }
             }
             return valid;
-        }else{
+        } else {
             return valid;
         }
     }
 
+    /**
+     * Method to handle the movement logic of a Player
+     *
+     * @param board             the current Board
+     * @param blockingSemaphore a Semaphore to control the state of the game
+     */
+    public abstract void move(Board board, Semaphore blockingSemaphore);
 
-    public Color getColor(){return this.color;}
+
+    public Color getColor() {
+        return this.color;
+    }
 }
